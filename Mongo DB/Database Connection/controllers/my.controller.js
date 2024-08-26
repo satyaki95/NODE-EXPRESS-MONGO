2dbbc4f4fc7d81e7a38f4342e7ca571b155bc836
satyaki95 <www.satyaki95@gmail.com>
2024-08-26 20:07:44 +0530

    get data from db

--- Mongo DB/Database Connection/controllers/my.controller.js
+++ Mongo DB/Database Connection/controllers/my.controller.js
@@ -21,3 +21,43 @@ exports.postProduct = async (req, res) => {
     });
   }
 };
+
+exports.getProductData = async (req, res) => {
+  try {
+    Product.find()
+      .limit(2)
+      .then((productData) => {
+        if (productData) {
+          res.status(200).send(productData);
+        } else {
+          res.status(404).send({
+            message: "No product found",
+          });
+        }
+      });
+  } catch (error) {
+    res.status(500).send({
+      error: error.message,
+    });
+  }
+};
+
+exports.getSpasificProductData = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Product.findOne({_id: id}).then((productData) => {
+      if (productData) {
+        res.status(200).send(productData);
+      } else {
+        res.status(404).send({
+          message: "No product found",
+        });
+      }
+    });
+  } catch (error) {
+    res.status(500).send({
+      error: error.message,
+    });
+  }
+};
--- Mongo DB/Database Connection/routes/my.route.js
+++ Mongo DB/Database Connection/routes/my.route.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { getProduct, postProduct } = require("../controllers/my.controller");
+const { getProduct, postProduct, getProductData, getSpasificProductData } = require("../controllers/my.controller");
 const router = express.Router();
 
 router.get("/", getProduct);
 
+//Get Product  Data
+router.get("/product", getProductData);
+router.get("/product/:id", getSpasificProductData);
+//Post  Product
 router.post("/products", postProduct);
 
 module.exports = router;
